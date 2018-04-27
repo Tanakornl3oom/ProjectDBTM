bdea2fe15d43e0b637a72800114e937247f04d5d
Tanakorn Peetivorapat <thanakorn_l3oomlnw@hotmail.com>
2018-04-27 14:18:42 +0700

    first commit

---
+++ projectDBTM/urls.py
@@ -0,0 +1,31 @@
+"""projectDBTM URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from divvy import views as core_views
+
+urlpatterns = [
+
+    path('', core_views.home, name='home'),
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, name='logout'),
+    path('signup/', core_views.signup, name='signup'),
+    path('settings/', core_views.settings, name='settings'),
+    path('settings/password/', core_views.password, name='password'),
+    path('auth/', include('social_django.urls', namespace='social')),
+    path('admin/', admin.site.urls),
+]
